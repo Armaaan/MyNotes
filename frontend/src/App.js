335927f408cf34601f79c019ad148908d692afff
Arman Ul Alam <armanulalam0@gmail.com>
2021-12-09 01:09:04 +0600

    Update and delete note

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import NotesListPage from "./pages/NotesListPage";
 import NotePage from "./pages/NotePage";
 
--- frontend/src/pages/NotePage.jsx
+++ frontend/src/pages/NotePage.jsx
@@ -1,16 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
-function NotePage({ match }) {
+function NotePage({ match, history }) {
   console.log(match);
-  const noteId = 1;
+  const noteId = match.params.id;
   const [note, setNote] = useState([]);
 
   const getNotes = async () => {
     const response = await fetch(`/api/notes/${noteId}`);
     const data = await response.json();
     setNote(data);
+  };
+
+  const updateNote = async () => {
+    await fetch(`/api/notes/${noteId}/update/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  const deleteNote = async () => {
+    await fetch(`/api/notes/${noteId}/delete`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application-json",
+      },
+    });
+    history.push("/");
+  };
+
+  const handleSubmit = () => {
+    updateNote();
+    history.push("/");
   };
 
   useEffect(() => {
@@ -21,12 +45,14 @@ function NotePage({ match }) {
     <div className="note">
       <div className="note-header">
         <h3>
-          <Link to="/">
-            <ArrowLeft />
-          </Link>
+          <ArrowLeft onClick={handleSubmit} />
         </h3>
+        <button onClick={deleteNote}>Delete</button>
       </div>
-      <textarea defaultValue={note?.body}></textarea>
+      <textarea
+        onChange={(e) => setNote({ ...note, body: e.target.value })}
+        defaultValue={note?.body}
+      ></textarea>
     </div>
   );
 }
