c24f3f7db74ef34609c786adb3013a4b7f5f4067
Arman Ul Alam <armanulalam0@gmail.com>
2021-12-18 03:10:36 +0600

    Update static files dirs

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -126,9 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+)
 
 
 CORS_ALLOW_ALL_ORIGINS = True
